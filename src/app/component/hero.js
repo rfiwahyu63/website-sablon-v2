2e7701bedbbaefcf4467a295427c6e5505f24708
Wahyu Rifai <wahyurifai63@gmail.com>
2026-07-26 11:24:48 +0700

    Menambahkan reponsive style

--- src/app/component/hero.js
+++ src/app/component/hero.js
@@ -8,40 +8,46 @@ const montserrat = Montserrat({
 
 export default function Hero() {
   return (
-    <section className="p-10 bg-mist-100 rounded-md shadow-[5px_5px_5px_rgba(0,0,0,0.2)]">
-      <div className="flex items-center">
+    <>
+    {/* mobile */}
+      <section className="p-10 lg:p-20 bg-slate-400 rounded-md shadow-[-3px_5px_3px_rgba(0,0,0,0.3)] ">
+        <div className="lg:flex items-center ml-8 lg:ml-20">
+          {/* Kiri */}
+          <div className="lg:w-1/2">
+            <h1
+              className={`${montserrat.className} text-3xl lg:text-5xl font-bold leading-tight mb-6`}
+            >
+              Cetak <span className="text-amber-400">Sablon Digital</span>{" "}
+              Berkualitas untuk Bisnis & Kebutuhan Personal
+            </h1>
 
-        {/* Kiri */}
-        <div className="w-1/2">
-          <h1
-            className={`${montserrat.className} text-5xl font-bold leading-tight mb-6`}
-          >
-            Cetak Sablon Digital Berkualitas untuk Bisnis & Kebutuhan Personal
-          </h1>
+            <p className="text-sm lg:text-xl text-gray-200 leading-relaxed mb-8">
+              Tuangkan kreativitas Anda dalam sebuah desain dan aplikasikan pada
+              kaos untuk kebutuhan branding maupun gaya pribadi. Hasil tajam,
+              warna awet, dan proses cepat.
+            </p>
 
-          <p className="text-xl text-gray-600 leading-relaxed mb-8">
-            Tuangkan kreativitas Anda dalam sebuah desain dan aplikasikan pada
-            kaos untuk kebutuhan branding maupun gaya pribadi. Hasil tajam,
-            warna awet, dan proses cepat.
-          </p>
+            <a
+              className="text-gray-200 italic px-4 py-2 rounded-xl border border-amber-400 hover:bg-amber-400 cursor-pointer transition delay-100"
+              href="#"
+            >
+              Pesan Sekarang
+            </a>
+          </div>
 
-          <a className="text-black px-4 py-2 rounded-lg border border-amber-500 hover:bg-amber-500 cursor-pointer transition-all"
-            href="#">
-            Pesan Sekarang
-          </a>
+          {/* Kanan */}
+
+          <div className="mt-10 lg:w-1/2 flex justify-center">
+            <Image
+              src="/assets/percetakan.jpg"
+              alt="percetakan"
+              width={500}
+              height={500}
+              className="object-cover rounded-xl border-gray-900 shadow-[15px_20px_8px_rgba(0,0,0,0.4)]"
+            />
+          </div>
         </div>
-
-        {/* Kanan */}
-      
-        <div className="w-1/2 flex justify-center">
-          <Image
-            src="/assets/image1.png"
-            alt="Hero"
-            width={300}
-            height={300}
-          />
-        </div>
-      </div>
-  </section>
+      </section>
+    </>
   );
 }
--- src/app/component/navbar.js
+++ src/app/component/navbar.js
@@ -1,6 +1,15 @@
+"use client";
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { House, Handshake, Images, Phone, ShoppingCart } from "lucide-react";
+import {
+  House,
+  Handshake,
+  Images,
+  Phone,
+  ShoppingCart,
+  Menu,
+} from "lucide-react";
 
 const menus = [
   {
@@ -29,56 +38,110 @@ const menus = [
     icon: ShoppingCart,
   },
 ];
-export default function Hero() {
+
+export default function Navbar() {
+  // Untuk reponsive sidebar
+  const [isOpen, setIsOpen] = useState(false);
+  //
   return (
-    <aside className=" flex flex-col items-center gap-20  w-56 h-screen bg-mist-100 border border-gray-200 shadow-[2px_0px_5px_rgba(0,0,0,0.2)]">
-      {/* Logo */}
-      <div className="border-b border-gray-300 pb-6 flex justify-center">
-        <Image
-          src="/assets/logo-rfidesigntrans.png"
-          alt="RFI Logo"
-          width={140}
-          height={50}
-          priority
+    <div>
+      <button
+        className="fixed p-2 m-5 bg-amber-400 rounded-2xl shadow-[-1px_1px_8px_rgba(0,0,0,0.4)] hover:text-white transition delay-200 cursor-pointer lg:hidden"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <Menu />
+      </button>
+      {isOpen && (
+        <div
+          className="fixed inset-0 bg-black/50 z-40 lg:hidden"
+          onClick={() => setIsOpen(false)}
         />
-      </div>
-
-      <div className="w-full">
-        <ul className="flex flex-col items-center gap-8">
-
-          {menus.map((menu) => {
-            const Icon = menu.icon;
-            return (
-              <Link
-                key={menu.name}
-                href={menu.href}
-                className="flex items-center font-bold italic gap-4 cursor-pointer hover:text-amber-500 transition-colors">         
-                <Icon className="w-5 h-5"/>
-                <span>{menu.name}</span>
-              </Link>
-            );
-          })}
-        </ul>
-      </div>
-
-      <div className="w-full flex flex-col p-2 ">
-        <a
-          className="text-center text-green-600 animate-floating text-2xl rounded-md font-bold cursor-pointer 
-                            relative
-                            after:content-['']
-                            after:block
-                            after:pb-2
-                            after:border-b
-                            after:border-amber-500
-                            after:scale-x-0
-                            after:transition-transform
-                            after:duration-200
-                            hover:after:scale-x-75"
-          href="#"
+      )}{" "}
+      <>
+        {/* sidebar mobile */}
+        <aside
+          className={`fixed top-0 left-0 w-48 h-screen lg:hidden bg-slate-400 z-50 gap-10 rounded-md border-gray-200 shadow-[3px_0px_5px_rgba(0,0,0,0.2)] transition-transform duration-700 ease-in-out ${isOpen ? "translate-x-0" : "-translate-x-full"} `}
+          onClick={() => setIsOpen(false)}
         >
-          Order Here !
-        </a>
-      </div>
-    </aside>
+          {/* Logo */}
+          <div className="border-b border-gray-50/50 pb-6 flex justify-center">
+            <Image
+              src="/assets/logo-rfidesigntrans.png"
+              alt="RFI Logo"
+              width={140}
+              height={50}
+              priority
+            />
+          </div>
+
+          <div className="w-full">
+            <ul className="flex flex-col items-center gap-15 mt-20">
+              {menus.map((menu) => {
+                const Icon = menu.icon;
+                return (
+                  <li
+                    className="relative after:content-[''] after:block after:pb-2 after:border-b after:border-amber-500 after:scale-x-0 after:transition-transform after:duration-200 hover:after:scale-x-75"
+                    key={menu.name}
+                  >
+                    <Link
+                      href={menu.href}
+                      className="flex items-center font-bold italic gap-4 cursor-pointer hover:text-amber-400 transition-colors"
+                    >
+                      <Icon className="w-5 h-5 text-red" />
+                      <span>{menu.name}</span>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </aside>
+
+        {/* sidebar dekstop */}
+        <aside className="hidden lg:flex flex-col w-56 h-screen bg-slate-400 gap-20 rounded-md border-gray-200 shadow-[3px_0px_5px_rgba(0,0,0,0.2)]">
+          {/* Logo */}
+          <div className="border-b border-gray-300/50 pb-6 flex justify-center">
+            <Image
+              src="/assets/logo-rfidesigntrans.png"
+              alt="RFI Logo"
+              width={140}
+              height={50}
+              priority
+            />
+          </div>
+
+          <div className="w-full">
+            <ul className="flex flex-col items-center gap-15">
+              {menus.map((menu) => {
+                const Icon = menu.icon;
+                return (
+                  <li
+                    className="relative after:content-[''] after:block after:pb-2 after:border-b after:border-amber-500 after:scale-x-0 after:transition-transform after:duration-200 hover:after:scale-x-75"
+                    key={menu.name}
+                  >
+                    <Link
+                      href={menu.href}
+                      className="flex items-center font-bold italic gap-4 cursor-pointer hover:text-amber-400 transition-colors"
+                    >
+                      <Icon className="w-5 h-5 text-red" />
+                      <span>{menu.name}</span>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+
+          <div className="w-full flex flex-col p-2 ">
+            <a
+              className="text-center text-amber-400 animate-floating text-2xl rounded-md font-bold cursor-pointer relative after:content-[''] after:block after:pb-2 after:border-b after:border-amber-500 after:scale-x-0 after:transition-transform after:duration-200 hover:after:scale-x-75"
+              href="#"
+            >
+              Order Now!
+            </a>
+          </div>
+        </aside>
+      </>
+    </div>
   );
 }
